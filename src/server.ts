19bac1afd996f02db7436de457aa4c1518e19d98
Fillipe Augusto <fillipe484@gmail.com>
2021-02-28 14:07:28 -0300

    post route and setting sqlite:

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
+import 'reflect-metadata';
 import express from 'express'; 
-
+import './database';
+import { router } from './router';
 const app = express();
 
-app.get("/", (request,response) => {
-  return response.json({message: "hello world"})
-});
-
-app.post("/", (request,response) => {
-  return response.json({message: "Dados salvos com sucesso  "})
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"))
